2b334bcf7800dfe9a5307088cea770063bc6fbca
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-03-03 14:57:34 -0300

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Francisco Manuel Gallegos Luque on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandboxTests/AccessibilitySandboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxTests.swift
+//  AccessibilitySandboxTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 03/03/2025.
+//
+
+import Testing
+@testable import AccessibilitySandbox
+
+struct AccessibilitySandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
